788f00fc8ca1699787bdfa6012f5199876eb5c71
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2025-11-08 23:40:45 -0800

    Added extra routes

--- app.js
+++ app.js
@@ -4,10 +4,34 @@ const app = express();
 
 const PORT = 3025;
 
+const inputs = [];
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.post('/confirmation', (req, res) => {
+    const forminput = {
+        fname: req.body.firstName,
+        lname: req.body.lastName,
+        title: req.body.title,
+        company: req.body.company,
+        LN: req.body.linkedIn,
+        EA: req.body.email,
+        meeting: req.body.meeting,
+        addition: req.body.Eaddition,
+        message: req.body.message,
+        format: req.body.format
+    }
+    inputs.push(forminput);
+
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+app.get('/admin', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/admin.html`, {inputs});
 });
 
 app.listen(PORT, () => {
