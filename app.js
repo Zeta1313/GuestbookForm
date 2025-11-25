205c25aaa0f63009c384bf1ea164d17f7088348b
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2025-11-25 00:40:51 -0800

    added SQL logic, currently failing after switch, need to figure out why

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}).promise();
+
 
 const PORT = 3025;
 
@@ -20,8 +32,10 @@ app.get('/form', (req, res) => {
     res.render('form');
 });
 
-app.post('/confirmation', (req, res) => {
-    const forminput = {
+app.post('/confirmation', async (req, res) => {
+    try {
+    const sql = 'INSERT INTO contacts fname, lname, title, company, LN, EA, meeting, addition, format, timestamps';
+        const forminput = {
         fname: req.body.firstName,
         lname: req.body.lastName,
         title: req.body.title,
@@ -34,12 +48,27 @@ app.post('/confirmation', (req, res) => {
         format: req.body.format
     }
     forminput.timestamp = new Date();
-    inputs.push(forminput);
-    res.render('confirmation', {forminput});
+
+    const [result] = await pool.execute(sql, forminput);
+
+    res.render('confirmation', {result});
+    }
+    catch(err) {
+        console.error('Database error:', err);
+        res.status(500).send('Database error: ' + err.message);
+    }
 });
 
-app.get('/admin', (req, res) => {
-    res.render('admin', {inputs});
+app.get('/admin', async (req, res) => {
+    try {
+        const [orders] = await pool.query('SELECT * FROM contacts');
+        res.render('admin', { orders: orders });
+    }
+    catch(err) {
+        console.error('Database error:', err);
+        res.status(500).send('Database error: ' + err.message);
+
+    }
 });
 
 app.listen(PORT, () => {
